945e27fe7002eecff3146044efd33755e7ea2c05
Dilanka Weerasinghe <Dilanka@localhost.localdomain>
2020-11-17 09:07:51 -0600

    Updates to DroneCamera

--- Sensor-Testing/DroneCamera.py
+++ Sensor-Testing/DroneCamera.py
@@ -1,28 +1,28 @@
 from picamera import PiCamera, Color
 import time
 
-camera = PiCamera()
-
 class DroneCamera:
-
     
     def __init__(self) : 
-        #camera = PiCamera()
+        self.camera = picamera.PiCamera()
 
-        camera.resolution = (1920,1080)
-        camera.framerate = 15
+        self.camera.resolution = (1920,1080)
+        self.camera.framerate = 15
 
-        camera.annotate_text = "Drone Video"
-        camera.annotate_background = Color('blue')
-        camera.annotate_foreground = Color('yellow')
+        self.camera.annotate_text = "Drone Video"
+        self.camera.annotate_background = Color('blue')
+        self.camera.annotate_foreground = Color('yellow')
+
+    def __del__(self):
+        self.camera.close()
 
     def takeVideo(self, length) :
-        camera.start_preview()
-        camera.start_recording('/home/pi/Videos/'+time.strftime("%S_%M_%d_%b")+'.h264')
+        self.camera.start_preview()
+        self.camera.start_recording('/home/pi/Videos/'+time.strftime("%S_%M_%d_%b")+'.h264')
         time.sleep(length)
-        camera.stop_recording()
-        camera.stop_preview()
+        self.camera.stop_recording()
+        self.camera.stop_preview()
     
 
 x = DroneCamera()
-x.takeVideo(10)
+x.takeVideo(4)

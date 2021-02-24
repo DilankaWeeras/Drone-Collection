7d0f2f00a58248a6456d2fe032d0ac01c1a950a8
Dilanka Weerasinghe <dweerasinghe@tamu.edu>
2021-02-24 09:42:51 -0600

    Fixed run.sh file

--- Drone-Flight/Flight/run.sh
+++ Drone-Flight/Flight/run.sh
@@ -1,5 +1,5 @@
 echo "to run use the promp --connect and then use the connection string"
-echo "Serial Port : /dev/ttyAMA0 , also set baud-57600"
+echo "Serial Port : /dev/ttyAMA0 , also set baud=57600"
 echo "SITL UDP : 127.0.0.1:14550"
 echo "Windows computer connected useing 3DR Telemetry Radio on COM14: com14 , also set baud=57600"
 python mission_main.py --connect udpin:0.0.0.0:14550

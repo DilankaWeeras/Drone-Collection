e377fec91acfb560389aa438d909d7dc9c5c536d
Dilanka <dweerasinghe@tamu.edu>
2021-03-25 13:20:03 -0500

    asdf

---
+++ Drone-Flight/Pi-Flight/pi_test_1.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
+import time
+import math
+from pymavlink import mavutil
+
+import argparse
+
+def connectToCopter():
+    parser = argparse.ArgumentParser(description='commands')
+    parser.add_argument('--connect')
+    args = parser.parse_args()
+
+    connection_string = args.connect
+    baud_rate = 57600
+
+    vehicle = connect(connection_string, baud = baud_rate, wait_ready=True)
+    return vehicle
+
+def arm():
+    while vehicle.is_armable==False:
+        print("Waiting for vehicle to initialise...")
+        time.sleep(1)
+    print("Vehicle is now Armable")
+    
+    vehicle.armed=True
+
+    while not vehicle.armed:      
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Vehicle is not armed.")
+    return None
+
+#MAIN
+
+vehicle = connectToCopter()
+arm()
+vehicle.armed = False()
+print("End of Script")

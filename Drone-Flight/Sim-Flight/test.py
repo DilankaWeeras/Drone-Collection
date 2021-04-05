1d3f5eaf935beb30e21b7e4f462c3b4fccfd4866
Dilanka <dweerasinghe@tamu.edu>
2021-04-05 00:44:04 -0500

    asdf

---
+++ Drone-Flight/Sim-Flight/test.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('test.log', 'a'))
+print = logger.info
+
+print('yo! Whats up')
